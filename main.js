cc31f99c979adf17975113ab4cf7116a4d5fddf4
Ryan Booth <ryanboothforever@gmail.com>
2021-02-13 22:12:40 -0600

    Removed row div for better responsiveness

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 //Make a way for when the user clicks a character's image, the name of that character is saved as a variable
-
 document.querySelectorAll('.char').forEach(item => {
     item.addEventListener('click', event => {
         if (event.localName = "img")
@@ -19,7 +18,6 @@ fetch(url)
                     //Once the character entered matches a charcter in the array, save the ID of the URL
                        url = url + data[i].id
                 }            
-            
     }   
     fetch(url) 
         .then(res=>res.json())
@@ -29,55 +27,8 @@ fetch(url)
             document.querySelector("#chBio").innerText = `${data.name} has ${data.hair_color.toLowerCase()} hair and ${data.eye_color.toLowerCase()} colored eyes.`
         })
            
-})  
-   
+})    
     .catch(err=>{
         console.log(`error ${err}`);
     })
 }
-
-
-
-
-/*Asynchronis code below*/
-
-// document.querySelector('#submit').addEventListener("click", getCharacter)
-
-// function getCharacter() {
-//     let character = document.querySelector('input').value
-//     let url = 'https://ghibliapi.herokuapp.com/people/'
-   
-//     fetch(url)
-//         .then(res => res.json())
-//         .then(data => {
-//             for (let i = 0; i < data.length; i++) {
-//                 if (character === data[i].name) {
-//                     //Once the character entered matches a charcter in the array, save the ID of the URL
-//                     url = url + data[i].id
-//                 }
-//             }
-//         })
-          
-//         .catch(err => {
-//             console.log(`error ${err}`);
-//         })
-// }
-
-// function getMovie(){
-//     let films = 'https://ghibliapi.herokuapp.com/films'
-//     fetch(films)
-//         .then(res => res.json())
-//         .then(data => {
-//             return data.title
-//         })
-//         .catch(err => {
-//             console.log(`error ${err}`);
-//         })
-// }
-
-
-// fetch()
-//     .then(res => res.json())
-//     .then(data => {
-//         document.querySelector("#chName").innerText = data.name;
-//         document.querySelector("#chBio").innerText = `${data.name} has ${data.hair_color} hair and ${data.eye_color} eyes and is a` + movie
